755a9bf6c91065b9d3ab33b20b15746b7d7cf5b3
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-08-09 15:04:14 -0500

    queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- DAY 1 -- 
+
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-01-01';
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
@@ -8,3 +10,12 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- DAY 2 --
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, AVG(escape_attempts) AS average_escapes FROM animals GROUP BY neutered;
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT NOT NULL,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL
+    weight_kg DECIMAL NOT NULL,
+    species VARCHAR(100)
 );
